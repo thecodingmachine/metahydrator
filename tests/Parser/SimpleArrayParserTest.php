2d979de7042bd9f141a2aa1b9d9c5a1ec0ff031e
lol <l.lebacq@thecodingmachine.com>
2018-11-06 16:04:50 +0100

    Add code covrage for function simpleArray

--- tests/Parser/SimpleArrayParserTest.php
+++ tests/Parser/SimpleArrayParserTest.php
@@ -11,17 +11,9 @@ class SimpleArrayParserTest extends \PHPUnit_Framework_TestCase
         $parser = new SimpleArrayParser();
 
         $this->assertSame(["random", "lambda", 42, null], $parser->parse(["random", "lambda", 42, null]));
-        $this->assertSame(["random", "lambda", 42], $parser->parse(["random", "lambda", 42]));
-        $this->assertSame(["random", "lambda"], $parser->parse(["random", "lambda"]));
-        $this->assertSame([21, 42], $parser->parse([21, 42]));
-        $this->assertSame([42], $parser->parse([42]));
-        $this->assertSame([], $parser->parse([]));
-
+        $this->assertSame(null, $parser->parse(null));
 
         $this->expectException(ParsingException::class);
         $parser->parse('');
-
-        $this->expectException(ParsingException::class);
-        $parser->parse(null);
     }
 }
